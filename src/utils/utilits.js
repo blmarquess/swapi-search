15dde1598860f7df9c62d8ee862487e1c195125f
Bruno <blmarques.dev@gmail.com>
2022-01-25 17:11:23 -0300

    Feat:(fetch) Cleaner results data

--- src/utils/utilits.js
+++ src/utils/utilits.js
@@ -1,6 +1,9 @@
 const URL_API = 'https://swapi-trybe.herokuapp.com/api/planets/';
 
-export const getDataAPI = (endpoint = URL_API) => (fetch(endpoint)
-  .then((response) => response.json()));
+export const getDataAPI = async (endpoint = URL_API) => {
+  const result = await (fetch(endpoint)
+    .then((response) => response.json()));
+  return result.results;
+};
 
 export const temp = 'json';
