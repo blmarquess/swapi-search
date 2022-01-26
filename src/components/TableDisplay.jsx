6fd42f5a932f7aea99fe2c80a471c5a3be31fa79
Bruno <blmarques.dev@gmail.com>
2022-01-26 17:43:08 -0300

    Feat:(tableDisplay) connect to contextAPI

--- src/components/TableDisplay.jsx
+++ src/components/TableDisplay.jsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import { DataContext } from '../context/DataProvider';
+import { TableHeader } from '../utils/utilits';
 
-const props = [];
+export default () => {
+  const { apiresults } = useContext(DataContext);
 
-export default function TableDisplay() {
   return (
     <table>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Residents</th>
+          {TableHeader.map((tdTitle) => (
+            <th key={ Math.random() }>{tdTitle}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
-        {
-          props.map((planet) => (
-            <tr key={ Math.random() }>
-              <td>{planet.name}</td>
-              <td>{planet.rotation_period}</td>
-            </tr>))
-        }
+        {apiresults.map((planet) => (
+          <tr key={ Math.random() }>
+            <td>{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>{planet.films}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>{planet.url}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
-}
-
-TableDisplay.propTypes = {
-  map: PropTypes.func.isRequired,
 };
--- src/context/DataContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const data = { dataapi: '{}' };
-
-const DataContext = React.createContext(data);
-
-export default DataContext;
