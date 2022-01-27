50feecdc5f3e0589b8cf1aadde1885496ca037f6
Bruno <blmarques.dev@gmail.com>
2022-01-27 00:57:03 -0300

    Feat: Sync ContextAPI and set name input functions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DataContext } from '../context/DataProvider';
 
-export default () => (
-  <>
-    <header>
-      <h1>
-        Projeto Star Wars Planets
-      </h1>
-      <section>
-        Search:
-        <input type="text" placeholder="Filtrar por" />
+export default () => {
+  const { setFilter, filterset } = useContext(DataContext);
+  return (
+    <>
+      <header>
+        <h1>
+          Projeto Star Wars Planets
+        </h1>
+        <section>
+          Search:
+          <input
+            type="text"
+            placeholder="Filtrar por"
+            data-testid="name-filter"
+            onChange={ ({ target }) => setFilter(target.name, target.value) }
+            name="planetName"
+            value={ filterset.planetName }
+          />
+        </section>
+      </header>
+
+      <section className="row-filter">
+        <select value="temp_popularity">
+          <option>
+            temp_popularity
+          </option>
+        </select>
+        <select value="temp_igual_ou">
+          <option>
+            temp_igual_ou
+          </option>
+        </select>
+        <input type="number" />
+        <button type="button">Filtrar</button>
+
+        <section>
+          <label htmlFor="ascend">
+            <input type="radio" id="ascend" name="order_filter" value="ascend" />
+            Ascendente
+          </label>
+          <label htmlFor="desend">
+            <input type="radio" id="desend" name="order_filter" value="desend" />
+            Descendente
+          </label>
+          <button type="button">Ordenar</button>
+        </section>
       </section>
-    </header>
-
-    <section className="row-filter">
-      <select value="temp_popularity">
-        <option>
-          temp_popularity
-        </option>
-      </select>
-      <select value="temp_igual_ou">
-        <option>
-          temp_igual_ou
-        </option>
-      </select>
-      <input type="number" />
-      <button type="button">Filtrar</button>
-
-      <section>
-        <label htmlFor="ascend">
-          <input type="radio" id="ascend" name="order_filter" value="ascend" />
-          Ascendente
-        </label>
-        <label htmlFor="desend">
-          <input type="radio" id="desend" name="order_filter" value="desend" />
-          Descendente
-        </label>
-        <button type="button">Ordenar</button>
-      </section>
-    </section>
-  </>
-);
+    </>
+  );
+};
